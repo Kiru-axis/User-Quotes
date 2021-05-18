3ef6f8fbbad009e709478b2f9ff79b6ab74b1d6f
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-18 15:37:55 +0300

    creating input for user

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,13 +8,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
-
+ 
   newQuote = new Quote(0," "," ",new Date());
   @Output() addQuote = new EventEmitter<Quote>();
 
   submitQuote(){
     this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote(0," "," ",new Date());
   }
 
   constructor() { }
